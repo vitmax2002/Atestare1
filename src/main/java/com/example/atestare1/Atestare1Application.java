b7427c1506859f68d61c75f0ba4fb76a4415b0a6
Coda Victor <vitmax.coda@gmail.com>
2023-11-06 17:12:53 +0200

    Crearea proiectului

---
+++ src/main/java/com/example/atestare1/Atestare1Application.java
@@ -0,0 +1,13 @@
+package com.example.atestare1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atestare1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Atestare1Application.class, args);
+    }
+
+}
